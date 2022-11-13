78d63f18809d308a552d6b08c16dcfa3cee9a847
Philip Turner <philipturner.AR@gmail.com>
2022-11-13 18:18:59 -0500

    Initial commit

---
+++ Tests/AIRVTests/AIRVTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import AIRV
+
+final class AIRVTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(AIRV().text, "Hello, World!")
+    }
+}
